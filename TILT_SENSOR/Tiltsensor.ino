8768be0c5564cbbd1995e2f41b359d9c6373498e
kartikcs17 <71758949+kartikcs17@users.noreply.github.com>
2020-11-25 12:58:12 +0530

    Add files via upload

---
+++ TILT_SENSOR/Tiltsensor.ino
@@ -0,0 +1,19 @@
+
+int tilt=4;
+int led=11;
+void setup()
+{
+  pinMode(tilt,INPUT);
+  pinMode(led,OUTPUT);
+}
+
+void loop()
+{
+  int reading;
+  reading=digitalRead(tilt);
+  if(reading)
+  digitalWrite(led, LOW);
+  else
+  digitalWrite(led, HIGH);
+
+}
